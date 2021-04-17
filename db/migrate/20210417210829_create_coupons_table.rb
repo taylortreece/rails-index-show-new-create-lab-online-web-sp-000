494e5ceb94405eb4df1adea7082bd27f357eb8bf
taylortreece <tayloratreece@gmail.com>
2021-04-17 17:16:38 -0500

    Done.

---
+++ db/migrate/20210417210829_create_coupons_table.rb
@@ -0,0 +1,10 @@
+class CreateCouponsTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :coupons do |t|
+      t.string "coupon_code"
+      t.string "store_name"
+
+      t.timestamps
+    end
+  end
+end
